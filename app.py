2f0a30db0820b07d8aaa27fafdf4ad866bcc4cfd
Paka <73528514+MEKIKAT@users.noreply.github.com>
2023-06-15 03:54:11 +0800

    added k-gram

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import string
 app = Flask(__name__)
 
 local_archive_directory = 'local_archive'
+k = 3 
 
 def detect_plagiarism(submitted_text):
     plagiarism_report = ""
@@ -33,12 +34,21 @@ def detect_plagiarism(submitted_text):
     return plagiarism_report
 
 def calculate_similarity(text1, text2):
+    k_grams1 = generate_k_grams(text1)
+    k_grams2 = generate_k_grams(text2)
 
-    words1 = set(text1.split())
-    words2 = set(text2.split())
-    common_words = words1.intersection(words2)
-    similarity = len(common_words) / len(words1)
+    common_k_grams = k_grams1.intersection(k_grams2)
+    similarity = len(common_k_grams) / len(k_grams1)
     return similarity
+
+def generate_k_grams(text):
+    k_grams = set()
+
+    for i in range(len(text) - k + 1):
+        k_gram = text[i:i+k]
+        k_grams.add(k_gram)
+
+    return k_grams
 
 @app.route('/')
 def index():
